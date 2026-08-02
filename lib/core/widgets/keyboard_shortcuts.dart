233eff8ee9e0d20f490a6cd97a6376c51ef445ff
defUserName-404 <moshiur.r.nafi@gmail.com>
2026-08-03 05:38:59 +0600

    fix(keyboard): remove space shortcut that blocked text input
    The bare Space key was mapped to push the focus session route via
    CallbackShortcuts, intercepting Space before text fields could receive
    it. Removed the binding entirely.

--- lib/core/widgets/keyboard_shortcuts.dart
+++ lib/core/widgets/keyboard_shortcuts.dart
@@ -4,7 +4,6 @@ import 'package:go_router/go_router.dart';
 
 import '../../features/projects/presentation/commands/project_commands.dart';
 import '../../features/tasks/presentation/commands/task_commands.dart';
-import '../routing/routes.dart';
 import '../utils/platform_utils.dart';
 
 class AppKeyboardShortcuts extends StatelessWidget {
@@ -22,9 +21,6 @@ class AppKeyboardShortcuts extends StatelessWidget {
         },
         SingleActivator(LogicalKeyboardKey.keyP, control: !meta, meta: meta): () {
           ProjectCommands.create(context);
-        },
-        const SingleActivator(LogicalKeyboardKey.space): () {
-          context.push(AppRoutes.focusSession.path);
         },
         const SingleActivator(LogicalKeyboardKey.escape): () {
           if (context.canPop()) context.pop();
